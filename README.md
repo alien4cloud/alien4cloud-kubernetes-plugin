129251e65eaa1401ce46a5f0e83415546393cb76
Luc Boutier <luc@fastconnect.fr>
2017-10-10 08:08:44 +0200

    Initial commit of kubernetes plugin.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alien4cloud kubernetes plugin
+
+Plugins that allows transformation of a TOSCA generic topology into a specific kubernetes topology.
+
+It contains support for topology and policies modification.
---
+++ src/main/java/org/alien4cloud/plugin/kubernetes/PluginConfiguration.java
@@ -0,0 +1,11 @@
+package org.alien4cloud.plugin.kubernetes;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ */
+@Configuration
+@ComponentScan(basePackages = "org.alien4cloud.plugin.kubernetes")
+public class PluginConfiguration {
+}
