aa6e3147a509a54655704d5d886340b8e7404a40
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-03-14 08:24:02 +0100

    [ALIEN-3405] fix versions of extended types & k8s types

--- src/main/java-templates/org/alien4cloud/plugin/kubernetes/csar/Version.java
+++ src/main/java-templates/org/alien4cloud/plugin/kubernetes/csar/Version.java
@@ -4,5 +4,5 @@ package org.alien4cloud.plugin.kubernetes.csar;
  * Contains plugin version.
  */
 public class Version {
-    public static final String K8S_CSAR_VERSION = "${project.version}";
+    public static final String K8S_CSAR_VERSION = "${org.alien4cloud.kubernetes.api.version}";
 }
